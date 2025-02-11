6621ec6efac6ef262dfef150f9ab8930eee79627
meebox <meebox@gmail.com>
2025-02-11 14:52:28 +0800

    update comments.

--- realtime_webrtc/secret_server.py
+++ realtime_webrtc/secret_server.py
@@ -6,16 +6,21 @@ app = Flask(__name__)
 client = OpenAI()
 
 def get_ephemeral_key():
+    # 生成臨時金鑰
     response = client.beta.realtime.sessions.create(
         model='gpt-4o-realtime-preview'
     )
+    # 把時效時間顯示在瀏覽器的終端機上
     print(f'Expires at: {time.ctime(response.client_secret.expires_at)}')
+    # 傳回臨時金鑰
     return response.client_secret.value
 
+# 顯示首頁
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# 取得臨時金鑰的路徑
 @app.route('/key')
 def key():
     return get_ephemeral_key()
